15001ea28a03009dc1ceb7426df8c8313448b69f
mateo <mateo.marchisone@gmail.com>
2023-01-25 18:36:37 -0300

    Formatted code

--- SorterWindow.py
+++ SorterWindow.py
@@ -3,26 +3,33 @@
 ################################################################################
 ## Form generated from reading UI file 'SorterWindow.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 6.4.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
+    QProgressBar, QPushButton, QSizePolicy, QStatusBar,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
-    def __init__(self):
-        self.lb = None
-
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(700, 390)
         MainWindow.setMinimumSize(QSize(700, 390))
         MainWindow.setMaximumSize(QSize(700, 390))
+        font = QFont()
+        font.setFamilies([u"Roboto"])
+        font.setPointSize(10)
+        MainWindow.setFont(font)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayoutWidget = QWidget(self.centralwidget)
@@ -35,6 +42,11 @@ class Ui_MainWindow(object):
         self.label.setObjectName(u"label")
         self.label.setMinimumSize(QSize(700, 0))
         self.label.setMaximumSize(QSize(700, 59))
+        font1 = QFont()
+        font1.setFamilies([u"Roboto"])
+        font1.setPointSize(15)
+        font1.setBold(True)
+        self.label.setFont(font1)
         self.label.setTextFormat(Qt.AutoText)
         self.label.setAlignment(Qt.AlignCenter)
 
@@ -58,7 +70,9 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2.addWidget(self.lb_src_dir)
 
+
         self.horizontalLayout.addLayout(self.horizontalLayout_2)
+
 
         self.verticalLayout_2.addLayout(self.horizontalLayout)
 
@@ -76,12 +90,14 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.lb_dst_dir)
 
+
         self.verticalLayout_2.addLayout(self.horizontalLayout_3)
 
         self.pb_go = QPushButton(self.verticalLayoutWidget)
         self.pb_go.setObjectName(u"pb_go")
 
         self.verticalLayout_2.addWidget(self.pb_go)
+
 
         self.verticalLayout.addLayout(self.verticalLayout_2)
 
@@ -101,6 +117,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_4.addWidget(self.progressBar)
 
+
         self.verticalLayout_3.addLayout(self.horizontalLayout_4)
 
         self.horizontalLayout_5 = QHBoxLayout()
@@ -110,7 +127,7 @@ class Ui_MainWindow(object):
         self.lb_current_number.setObjectName(u"lb_current_number")
         self.lb_current_number.setMinimumSize(QSize(0, 10))
         self.lb_current_number.setMaximumSize(QSize(150, 30))
-        self.lb_current_number.setAlignment(Qt.AlignLeading | Qt.AlignLeft | Qt.AlignVCenter)
+        self.lb_current_number.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
 
         self.horizontalLayout_5.addWidget(self.lb_current_number)
 
@@ -126,7 +143,9 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_5.addWidget(self.lb_total_number)
 
+
         self.verticalLayout_3.addLayout(self.horizontalLayout_5)
+
 
         self.verticalLayout.addLayout(self.verticalLayout_3)
 
@@ -142,7 +161,6 @@ class Ui_MainWindow(object):
         self.pb_go.clicked.connect(MainWindow.go_slot)
 
         QMetaObject.connectSlotsByName(MainWindow)
-
     # setupUi
 
     def retranslateUi(self, MainWindow):
@@ -158,3 +176,4 @@ class Ui_MainWindow(object):
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"/", None))
         self.lb_total_number.setText(QCoreApplication.translate("MainWindow", u"...", None))
     # retranslateUi
+
